44acd022a91a6533170b34a74227d8e67b602bbc
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-01-22 14:02:12 +0530

    python

--- first.py
+++ first.py
@@ -1,3 +1,13 @@
-print("hello world")
-name=input("name :")
-print(name)
+# print("hello world")
+# name=input("name :")
+# print(name)
+
+light=input("light colour :")
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look")
+elif(light == "green"):
+    print("go")
+else:
+    print("light is broken")
