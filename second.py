5f74f382e66e14a29d9f5838bbb582022f23a409
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-01-24 13:15:15 +0530

    start

--- second.py
+++ second.py
@@ -5,10 +5,18 @@
 # print(str[0]+str[2])
 # print(str2[1:9])
 
-list = [2,3,4,1]
-list.append(9)
-print(list)
-list.sort()
-print(list)
-list.sort(reverse=True)
-print(list)
+# list = [2,3,4,1]
+# list.append(9)
+# print(list)
+# list.sort()
+# print(list)
+# list.sort(reverse=True)
+# print(list)
+
+# a = str(input("enter fav movie1 :"))
+# b = str(input("enter fav movie2 :"))
+# c = str(input("enter fav movie3 :"))
+
+# list=[ a, b, c]
+# print(list)
+
