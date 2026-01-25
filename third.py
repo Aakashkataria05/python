e25e68438761a3359f88f31e340caaea116ef6ba
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-01-25 11:46:25 +0530

    yoyo

---
+++ third.py
@@ -0,0 +1,12 @@
+student1 = {
+    "name" : "aakash",
+    "class" : "12th standard",
+    "grade" : "A+",
+    "contact" : "93508xxxxx",
+    "moto" : "gaand fadna"
+}
+
+# print(student1["moto"])
+
+collection = { 1, 5, 5, 5, 6, "jai", "baba ki", 9}
+print(collection)
