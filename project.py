b566e3baf3d3f6906b6960cd6f4a331f232bd61b
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-02-01 13:49:12 +0530

    guess

--- project.py
+++ project.py
@@ -2,4 +2,17 @@ import random
 
 target = random.randint(1,100)
 
-print(target)
+while True:
+    userchoice = int(input("guess the number :-"))
+
+    if(userchoice == target):
+        print("Success : correct guess !!")
+        break
+    
+    elif(userchoice > target):
+        print("wrong guess : guess smaller numbeer")
+
+    else:
+        print("wrong guess : guess bigger numbeer")
+
+print("-----GAME OVER-----")
