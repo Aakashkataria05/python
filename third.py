d26b0f55b37fa9a69edd1c39d8d0c5a3730f83da
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-02-03 13:18:14 +0530

    yoyo

--- third.py
+++ third.py
@@ -1,13 +1,22 @@
-student1 = {
-    "name" : "aakash",
-    "class" : "12th standard",
-    "grade" : "A+",
-    "contact" : "93508xxxxx",
-    "moto" : "gaand fadna"
-}
+# student1 = {
+#     "name" : "aakash",
+#     "class" : "12th standard",
+#     "grade" : "A+",
+#     "contact" : "93508xxxxx",
+#     "moto" : "gaand fadna"
+# }
 
-print(student1["moto"])
-print(student1["grade"])
+# print(student1["moto"])
+# print(student1["grade"])
 
-collection = { 1, 5, 5, 5, 6, "jai", "baba ki", 9}
-print(collection)
+# collection = { 1, 5, 5, 5, 6, "jai", "baba ki", 9}
+# print(collection)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+        # Return an empty list if no solution is found
+        return []
