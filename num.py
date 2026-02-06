27d8e21062275665b3797fa06bfa1d681f65ae27
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-02-06 15:14:56 +0530

    tt

--- num.py
+++ num.py
@@ -1,11 +1,14 @@
 import numpy as np
 numpy_array = np.array([1,2,3,4,5])
-print(numpy_array)
+# print(numpy_array)
 
-ones_array = np.ones((2,3))
-print(ones_array)
+# ones_array = np.ones((2,3))
+# print(ones_array)
 
-filled_array = np.full((2,4),9)
-print(filled_array.shape)
-print(filled_array.size)
-print(filled_array.astype(str))
+# filled_array = np.full((2,4),9)
+# print(filled_array.shape)
+# print(filled_array.size)
+# print(filled_array.astype(str))
+
+new_arr = np.insert(numpy_array, 3, 100)
+print(new_arr)
