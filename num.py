d369e907eb31a24f8d604f9e3c34fc0ac312addd
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-02-06 16:10:55 +0530

    num

--- num.py
+++ num.py
@@ -12,3 +12,9 @@ numpy_array = np.array([1,2,3,4,5])
 
 new_arr = np.insert(numpy_array, 3, 100)
 print(new_arr)
+
+arr1 = np.array([5,6,7])
+arr2 = np.array([5,4,3])
+
+result = arr1 + arr2
+print(result)
