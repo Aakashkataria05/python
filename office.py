26a90ec4e782d7fdbd1b6c56ca1bfe379952cd77
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-02-08 22:33:47 +0530

    office

--- office.py
+++ office.py
@@ -15,3 +15,12 @@ class engineer(employe):
 
 e1 = employe("aakash","data scientist",520000)
 e1.show_details()
+
+new_arr = np.insert(numpy_array, 3, 100)
+print(new_arr)
+
+arr1 = np.array([5,6,7])
+arr2 = np.array([5,4,3])
+
+result = arr1 + arr2
+print(result)
