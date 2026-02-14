a5604900b8164cd227cc40ffda2b719409b4f7fb
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-02-14 15:51:59 +0530

    holiday

--- num2.py
+++ num2.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 arr = np.array([1,2,3,4,5,6])
-print(arr.dtype)
-print(arr.astype(float))
+# print(arr.dtype)
+# print(arr.astype(float))
+
+# print(arr*2)
+print(np.mean(arr))
+print(np.argmax(arr))
+print(np.argmin(arr))
