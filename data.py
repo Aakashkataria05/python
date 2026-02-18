5de9125f99837193905893c0a38113e4bd5b79c5
Aakashkataria05 <149020636+Aakashkataria05@users.noreply.github.com>
2026-02-18 13:31:47 +0530

    kk

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
 import pandas as pd 
 
 df = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
-print(df)
+print(df.info())
+print(df.shape)
+print(df.describe())
+
